bc4f0bb0e460d71d6ce6b619ecf92533cefc9fcd
jumper199 <lamperski+github@gmail.com>
2020-01-07 22:01:06 +0000

    changed port to 80

--- flask/runserver.py
+++ flask/runserver.py
@@ -4,4 +4,4 @@ from notejam.config import DevelopmentConfig
 app.config.from_object(DevelopmentConfig)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
